34e1ebcfb70caa7ef08edea2afb83bade2782c3c
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2019-04-22 00:37:57 +0200

    Python 2 compatible

--- django_csrf_protect_form/__init__.py
+++ django_csrf_protect_form/__init__.py
@@ -11,12 +11,12 @@ __version__ = '.'.join(map(str, VERSION))
 class CsrfProtectFormMiddleware(CsrfViewMiddleware):
     def __init__(self, get_response=None):
         self.CSRF_PROTECT_FORM_CONTENT_TYPE = getattr(settings, 'CSRF_PROTECT_FORM_CONTENT_TYPE', ['application/x-www-form-urlencoded', 'multipart/form-data', 'text/plain'])
-        super().__init__(get_response)
+        super(CsrfProtectFormMiddleware, self).__init__(get_response)
 
     def process_request(self, request):
         if request.content_type not in self.CSRF_PROTECT_FORM_CONTENT_TYPE:
             request.csrf_processing_done = True
-        super().process_request(request)
+        super(CsrfProtectFormMiddleware, self).process_request(request)
 
     def process_response(self, request, response):
         if getattr(response, 'csrf_cookie_set', False) and response['Content-Type'] not in self.CSRF_PROTECT_FORM_CONTENT_TYPE:
